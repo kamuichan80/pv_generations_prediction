e84dc997d1d46cf419374ac6565f2ff1979927cc
Byounghoon Lee <kamuichan80@gmail.com>
2021-08-23 12:54:46 +0900

    Add files via upload

--- error_cal.py
+++ error_cal.py
@@ -27,6 +27,6 @@ def error_cal(lat: str, lng: str, strSDate: str, strEDate: str, strOrgCd: str, p
     return df_nmae, df_nmae_hour, df_nmae_date
 
 
-print(error_cal(37.4772, 126.6249, '2021-08-16', '2021-08-22', '876', 'Incheon', 200))
+# print(error_cal(37.4772, 126.6249, '2021-08-16', '2021-08-22', '876', 'Incheon', 200))
 # print(error_cal(35.10468, 129.0323, '2021-08-16', '2021-08-22', '997N', 'Busan_1', 115)) 
 # print(error_cal(35.10468, 129.0323, '2021-08-16', '2021-08-22', '997G', 'Busan_2', 187)) 
